24f07e59f6dca79088308f04c1feea321f33fc54
Ekeopara-Praise <ekeoparapraise@gmail.com>
2022-12-21 14:16:12 +0100

    Made some code refactoring

--- IsochronalTest.py
+++ IsochronalTest.py
@@ -1,12 +1,25 @@
-import pandas as pd
+# import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# create the Isochronal test class
 class Isochronal:
+    """
+        Isochronal class that calculates the well deliveribility potential of a gas well \
+        when given the required input parameters.
 
+        Args:
+             data (pandas dataframe) --> The Isochronal well test data
+             Pr (float dtype) --> Reservoir Pressure, Psi of the well
+             q_column (str dtype)--> flowrate column title in the data
+             psd_column (str dtype) --> Pressure squared column title in the data
+             Qstable (float dtype) --> stabilized flowrate, Mscf/d
+             Pwfstable (float dtype) --> stabilized bottomhole flowing pressure, Psi
+    """
     def __init__(self, data, Pr, q_column, psd_column, Qstable, Pwfstable):
 
+        # initialize the input parameters
         self.data = data
         self.q_column = q_column
         self.psd_column = psd_column
@@ -14,32 +27,79 @@ class Isochronal:
         self.Pwfstable = Pwfstable
         self.Pr = Pr
 
+        # globalize slope and intercept
         global slope
         global intercept
 
+        # calculate the slope and the intercept values
         slope = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[0]
         intercept = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[1]
 
     def n_factor(self):
+        """
+           Method that calculates the n-factor of the Isochronal test.
 
-        return round(1 / slope, 4)
+           Args:
+               None
+
+           Returns:
+               float dtype: n-factor value
+        """
+        n = 1 / slope
+
+        return round(n, 4)
 
     def c_factor(self):
+        """
+           Method that calculates the c-factor of the Isochronal test.
 
-        return round(self.Qstable / (self.Pr ** 2 - self.Pwfstable ** 2) ** self.n_factor(), 4)
+           Args:
+               None
 
-    def IPR_equation(self):
+           Returns:
+               float dtype: c-factor value
+                """
+        c = self.Qstable / (self.Pr ** 2 - self.Pwfstable ** 2) ** self.n_factor()
 
-        return f"Qg = {round(self.c_factor(), 4)}(Pr^2 - Pwf^2)^{round(self.n_factor(), 4)}"
+        return round(c, 4)
+
+    def IPR(self):
+        """
+           Method outputs the Inflow Performance Relation of the Isochronal test.
+
+           Args:
+               None
+
+           Returns:
+               str dtype: IPR equation
+        """
+        return f"Qg = {self.c_factor()}(Pr^2 - Pwf^2)^{self.n_factor()}"
 
     def pressure_squared_graph(self):
+        """
+           Method displays the pressure squared against the flowrate
 
+           Args:
+               None
+
+           Returns:
+               image: a graphical representation
+        """
+        # set up the figure sizes
         plt.figure(figsize=(8, 6))
+        plt.style.use('grayscale')
+
+        # plot of the log-log graph
         plt.loglog(self.data[self.q_column], self.data[self.psd_column])
 
+        # the scatter plot
         plt.scatter(self.data[self.q_column], self.data[self.psd_column], color='r')
-        plt.xlabel('Qg, Mscf/d')
-        plt.ylabel('Pressure squared, psi^2')
+
+        # define the labels for the graph
+        plt.title("Isochronal Plot (Using Pressure Squared Approach)", fontweight='bold', fontsize=20)
+        plt.xlabel('Qg, Mscf/d', fontweight='bold', fontsize=15)
+        plt.ylabel('Pressure squared, psi^2', fontweight='bold', fontsize=15)
+        plt.grid(visible=True)
         plt.show()
 
     def AOF(self):
@@ -47,15 +107,3 @@ class Isochronal:
         Qmax = self.c_factor() * ((self.Pr ** 2 - 14.73 ** 2) ** self.n_factor())
 
         return round(Qmax, 4)
-
-
-df = pd.read_csv('test_data.csv')
-df['psq'] = 1798 ** 2 - df['P'] ** 2
-print(df.head())
-
-iso = Isochronal(df, 1798, 'Qg', 'psq', 6300, 1600)
-print("n =", iso.n_factor())
-print("c =", iso.c_factor())
-print("IPR equation is", iso.IPR_equation())
-print("AOF =", iso.AOF())
-print(iso.pressure_squared_graph())
--- testycodes.py
+++ testycodes.py
@@ -1,23 +1,52 @@
-class GasTest:
+import pandas as pd
+from IsochronalTest import Isochronal
 
-    def __init__(self, dataset=None):
-        self.dataset = dataset
+df = pd.read_csv('test_data.csv')
+df['psq'] = 1798 ** 2 - df['P'] ** 2
+print(df.head())
 
-        if dataset is None:
-            self.dataset = []
-        # self.data = self.dataset
-        self.dataset = self.dataset
+iso = Isochronal(df, 1798, 'Qg', 'psq', 6300, 1600)
+print(f"n = {iso.n_factor()}")
+print(f"c = {iso.c_factor()}")
+print(f"IPR equation is: {iso.IPR()}")
+print(f"AOF = {iso.AOF()}Mscf/d", )
+print(iso.pressure_squared_graph())
 
 
-# slope = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[0]
-        # intercept = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[1]
-        #
-        # def abline(slope, intercept):
-        #     axes = plt.gca()
-        #     x_vals = np.array(axes.get_xlim())
-        #     y_vals = intercept + slope * x_vals
-        #     plt.plot(x_vals, y_vals, '--')
-        #
-        # plt.scatter(np.log(self.data[q_column]), np.log(self.data[psd_column]))
-        # print(abline(slope, intercept))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class GasTest:
+#
+#     def __init__(self, dataset=None):
+#         self.dataset = dataset
+#
+#         if dataset is None:
+#             self.dataset = []
+#         # self.data = self.dataset
+#         self.dataset = self.dataset
+#
+#
+# # slope = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[0]
+#         # intercept = np.polyfit(np.log(self.data[q_column]), np.log(self.data[psd_column]), 1)[1]
+#         #
+#         # def abline(slope, intercept):
+#         #     axes = plt.gca()
+#         #     x_vals = np.array(axes.get_xlim())
+#         #     y_vals = intercept + slope * x_vals
+#         #     plt.plot(x_vals, y_vals, '--')
+#         #
+#         # plt.scatter(np.log(self.data[q_column]), np.log(self.data[psd_column]))
+#         # print(abline(slope, intercept))
 
